84d166120eeb7c601dacc04ae13f7acfcc634f4c
Irreversible Reboot <irreversible@reboot.moe>
2019-12-29 15:58:42 +0100

    Refactor for easier management of new media categories
    Also changed all references of image to media since the bot now posts
    GIFs and videos.

--- post.rb
+++ post.rb
@@ -14,21 +14,43 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = bot_config['access_token_secret']
 end
 
-folder_list = Pathname.new(bot_config['image_path']).children.select { |c| c.directory? }
+folder_list = Pathname.new(bot_config['media_path']).children.select { |c| c.directory? }
 folder = folder_list.sample(1)[0]
 
-image = Pathname.new(folder).children.sample(1)[0]
+media = Pathname.new(folder).children.sample(1)[0]
 
-unless image.nil?
+unless media.nil?
 	
-	if folder.to_s.include?("episode")
-		episode_number = folder.basename.to_s.match(/[0-9][0-9]/)[0].to_i.to_s
-		client.update_with_media("#{episode_number}話のあいねちゃん", image.open)
-	else
-		client.update_with_media("", image.open)
+	tweet_message = ""
+
+	case folder.basename.to_s.match(/([a-z]+)/)[0]
+
+	when "episode"
+		episode_number 	= folder.basename.to_s.match(/[0-9][0-9]/)[0].to_i
+		tweet_message 	= "#{episode_number}話のあいねちゃん"
+
+	when "opening"
+		opening_number 	= folder.basename.to_s.match(/[0-9]/)[0].to_i
+		
+		case opening_number
+		when 1
+			tweet_message 	= "OP曲ありがと⇄大丈夫のあいねちゃん"
+		end
+	
+
+	when "ending"
+		ending_number 	= folder.basename.to_s.match(/[0-9]/)[0].to_i
+
+		case ending_number
+		when 1
+			tweet_message 	= "ED曲Believe itのあいねちゃん"
+		end
+
+	
 	end
 
-	logger.info "Posted image #{image.basename} from #{folder.basename}"	
+	client.update_with_media(tweet_message, media.open)
+	logger.info "Posted media #{media.basename} from #{folder.basename}"	
 
 else
 	logger.info "Folder #{folder.basename} was empty!"
