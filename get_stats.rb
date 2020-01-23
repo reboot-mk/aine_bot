12b0f1e40d3f25e522f6c9808a2b0be9b5e46e6f
Irreversible Reboot <irreversible@reboot.moe>
2020-01-23 12:33:22 +0100

    Forgot the get_stats file ooops

---
+++ get_stats.rb
@@ -0,0 +1,7 @@
+require 'json'
+require_relative "aine_bot"
+
+config = JSON.parse(File.read('config.json'))
+
+bot = AineBot.new(config['media_path'], config['consumer_key'], config['consumer_secret'], config['access_token'], config['access_token_secret'])
+puts bot.get_stats
